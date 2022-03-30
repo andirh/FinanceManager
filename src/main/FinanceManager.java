5e7c64fc14496e4a0b869e568a05b554e2761e16
andirh <57002449+andirh@users.noreply.github.com>
2022-03-30 09:48:10 +0200

    Changed project structure

--- src/main/FinanceManager.java
+++ src/main/FinanceManager.java
@@ -1,4 +1,21 @@
 package main;
 
+import main.model.Account;
+import main.model.Owner;
+
 public class FinanceManager {
+
+    public static void main(String args[]){
+
+        Owner owner = new Owner("Andreas", "Rheinwalt");
+
+        Account account = new Account(12345678, owner);
+
+        account.increaseBalance(50);
+        System.out.println(account.getBalance());
+        account.decreaseBalance(10);
+        System.out.println(account.getBalance());
+    }
+
+
 }
